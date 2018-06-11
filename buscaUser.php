e6004e0a71024c233e3161b49ef5658837ea3551
jerry <jerry.carlos.xd@gmail.com>
2018-06-11 02:02:34 -0300

    Sinapse 11/06/2017

--- buscaUser.php
+++ buscaUser.php
@@ -1,7 +1,7 @@
 <?php
 if($_SERVER['REQUEST_METHOD'] == "POST"){
-	require "./config/defineDB.php";
-	require "./config/conectaBanco.class.php";	
+	require "../config/defineDB.php";
+	require "../config/conectaBanco.class.php";	
 	$banco = new ConectaBanco();
 	$banco->conectaBanco($dbserver,$dbuser,$dbpwd,$dbuse);
 	$data = json_decode(file_get_contents('php://input'), true);		
@@ -15,13 +15,16 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 			$sql = "SELECT * from usuario where (email = '$email' or login = '$email') and senha = '$senha'";
 			$consulta = $banco->executa($sql);
 			if(mysqli_num_rows ($consulta) > 0){				
-				while($registro = mysqli_fetch_assoc($consulta)){
+				$registro = mysqli_fetch_assoc($consulta);
 					//$json[] = $registro;
-					$json = array("id"=>$registro['id'],"nome"=>$registro['nome'],"email"=>$registro['email'],"senha" =>$registro['senha'],"login"=>$registro['login'],"telefone"=>$registro['telefone'],"instituicao"=>$registro['instituicao'],"curso"=>$registro['curso'],"periodo"=>$registro['periodo'],"ocupacao"=>$registro['ocupacao'] );
 
-				}				
-			}else $json = array("erro" => 0, "msg" => "Senha incorreta!");
-		}else $json = array("erro" => -1, "msg" => "Credencial não cadastrada!");
+				$id = $registro['id'];
+				$sql = "SELECT imagem from perfil where fk_user = $id";
+				$imagem = $banco->executa($sql);
+				$img = mysqli_fetch_assoc($imagem);
+					$json = array("id"=>$registro['id'],"nome"=>$registro['nome'],"email"=>$registro['email'],"senha" =>$registro['senha'],"login"=>$registro['login'],"telefone"=>$registro['telefone'],"instituicao"=>$registro['instituicao'],"curso"=>$registro['curso'],"periodo"=>$registro['periodo'],"ocupacao"=>$registro['ocupacao'],"imagem"=>$img['imagem'],"status"=>1,"msg"=>"Logado com sucesso");							
+			}else $json = array("status" => -1, "msg" => "Senha incorreta!");
+		}else $json = array("status" => -1, "msg" => "Credencial não cadastrada!");
 }else{
 	$json = array("status" => 0, "msg" => "Metodo nao post!");
 }
--- cadastroUsuario.php
+++ cadastroUsuario.php
@@ -1,8 +1,8 @@
 <?php
 if($_SERVER['REQUEST_METHOD'] == "POST"){
 ini_set('default_charset', 'UTF-8');	
-	include "./config/defineDB.php";
-require ("./config/conectaBanco.class.php");
+	include "../config/defineDB.php";
+require ("../config/conectaBanco.class.php");
 	$banco = new ConectaBanco();
 	$banco->conectaBanco($dbserver,$dbuser,$dbpwd,$dbuse);
 	$data = json_decode(file_get_contents('php://input'), true);		
@@ -15,31 +15,35 @@ require ("./config/conectaBanco.class.php");
 		$curso = $data["curso"];
 		$periodo = $data["periodo"];
 		$ocupacao = $data["ocupacao"];
+		$foto = $data['foto'];
 		$strErro = "";
 		$erro = 0;		
 		$sql = "SELECT id from usuario where (email = '$email')";		
 		$consulta = $banco->executa($sql);	
+		
 		if(mysqli_num_rows ($consulta) > 0){				
 			$erro++;
-			$strErro = $strErro."[ Email";
+			$strErro = "[ Email";
 			
 		}
 
 		$sql = "SELECT id from usuario where (login = '$login')";		
 		$consulta = $banco->executa($sql);	
+		
 		if(mysqli_num_rows ($consulta) > 0){
 			if($erro > 0)
-				$strErro = $strErro.", login";
+				$strErro += ", login";
 			else $strErro = "[ Login";				
 			$erro++;			
 		}
 
 		$sql = "SELECT id from usuario where (telefone = '$telefone')";		
 		$consulta = $banco->executa($sql);	
+
 		if(mysqli_num_rows ($consulta) > 0){	
 			if($erro > 0){
 				$erro++;
-				$strErro = $strErro.", telefone";
+				$strErro += ", telefone";
 			}
 			else {
 				$erro = 1;
@@ -53,11 +57,27 @@ require ("./config/conectaBanco.class.php");
 			if($result){		
 				$sql = "SELECT * from usuario where email = '$email'";
 				$consulta = $banco->executa($sql);
-				while($registro = mysqli_fetch_assoc($consulta)){
-					//$json[] = $registro;
-					$json = array("id" => $registro['id'],"nome" => $registro['nome'],"email" => $registro['email'],"senha" => $registro['senha'],"login" => $registro['login'],"telefone" => $registro['telefone'],"instituicao" => $registro['instituicao'],"curso"=>$registro['curso'],"periodo" => $registro['periodo'],"ocupacao" => $registro['ocupacao'],"status" => 1, "msg" => "Usuario cadastrado com sucesso!");
+				$registro = mysqli_fetch_assoc($consulta);
 
-				}				
+				//while($registro = mysqli_fetch_assoc($consulta)){
+					//$json[] = $registro;
+					$id = $registro['id'];
+					$idImg = uniqid().$id;															
+					if(salvarFoto($foto,$idImg)){		
+						$sql = "INSERT INTO perfil (id,imagem, fk_user) values (default,'$idImg','$id')";
+						$banco->executa($sql);				
+						$json = array("id" => $registro['id'],"nome" => $registro['nome'],"email" => $registro['email'],"senha" => $registro['senha'],"login" => $registro['login'],"telefone" => $registro['telefone'],"instituicao" => $registro['instituicao'],"curso"=>$registro['curso'],"periodo" => $registro['periodo'],"ocupacao" => $registro['ocupacao'],"status" => 1, "msg" => "Usuario cadastrado com sucesso!","imagem" => $idImg);
+					}else{
+						$delUser = "DELETE FROM usuario where id = $id";
+						$delImg = "DELETE FROM perfil where fk_user = $id";
+						
+						if($banco->executa($delImg) === TRUE)
+							$banco->executa($delUser);
+
+						$json = array("status" => 0, "msg" => "Foto não foi salva no servidor, tente novamente!");
+					}
+
+				//}				
 			}else{
 				$json = array("status" => 0, "msg" => "Usuário não podê ser cadastrado!");
 			}
@@ -74,5 +94,16 @@ require ("./config/conectaBanco.class.php");
 }
 header('Content-type: application/json');
 echo json_encode($json);
+
+		function salvarFoto($img,$id){
+			define('UPLOAD_DIR', 'profiles/');
+			$img = str_replace('data:image/png;base64,', '', $img);
+			$img = str_replace(' ', '+', $img);
+			$data = base64_decode($img);
+			//$id = uniqid();
+			$file = UPLOAD_DIR . $id . '.png';
+			$success = file_put_contents($file, $data);
+			return $success ? true : false;
+		}
 
  ?>
